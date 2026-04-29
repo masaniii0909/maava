9cf06fe5075dfeb38ec833f9b9f8a1c5dfdf81f2
mn <164825732+masaniii0909@users.noreply.github.com>
2026-04-29 10:03:38 -0400

    python_weather is stupid
    wwo better

--- TOOLS/weather.py
+++ TOOLS/weather.py
@@ -1,21 +1,29 @@
 import argparse as ap
-from requests_html import HTMLSession
-import asyncio
-import python_weather
+import requests
+import rich
+
+apikey = "" # put api key here if you want weather
+url  = "https://api.worldweatheronline.com/premium/v1/weather.ashx"
+
 parser = ap.ArgumentParser(description='get the weather for an area')
 parser.add_argument('--prompt')
 args = parser.parse_args()
 prompt = args.prompt
+
 prompt = prompt.split(' in ')[1]
 print(prompt)
-async def main() -> None:
-  
-  async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
-    
-    weather = await client.get(f'{prompt}')
-    
-    print(weather.temperature)
-    
-if __name__ == '__main__':
-  asyncio.run(main())
 
+def get_weather(location):
+    params = {
+        "key":apikey,
+        "q":location,
+        "format": "json",
+        "includelocation":"yes",
+        "cc":"yes",
+        
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data["data"]
+weather = get_weather(f"{prompt}")
+print(weather["current_condition"][0]["temp_F"], "F")
